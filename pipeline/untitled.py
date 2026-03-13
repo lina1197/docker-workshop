0770d0b6e32b985757128ae9ddb0df85a99632cd
lina1197 <119867186+lina1197@users.noreply.github.com>
2026-03-13 00:45:50 +0000

    latest commit

--- pipeline/untitled.py
+++ pipeline/untitled.py
@@ -1,21 +1,15 @@
+docker run -it --rm   -e POSTGRES_USER="root"   -e POSTGRES_PASSWORD="root"   -e POSTGRES_DB="ny_taxi"   -v ny_taxi_postgres_data:/var/lib/postgresql   -p 5432:5432   --network=pg-network   --name pgdatabase   postgres:18
 
+docker run -it  --rm --network=pg-network taxi_ingest:v001 \
+  --pg_user=root --pg_pass=root --pg_host=pgdatabase \
+  --pg_port=5432 --pg_db=ny_taxi \
+  --target_table=yellow_taxi_trips_2021_1 --chunksize=100000
 
-docker run -it --rm \ 
---network=
-    taxi_ingest:v001 \ 
-        --pg_user=root \
-        --pg_pass=root \
-        --pg_host=localhost \
-        --pg_port=5432 \
-        --pg_db=ny_taxi \
-        --target_table=yellow_taxi_trips_2021_1 \
-        --chunksize=100000
-
-uv run python ingest_data.py \
-    --pg_user=root \
-    --pg_pass=root \
-    --pg_host=localhost \
-    --pg_port=5432 \
-    --pg_db=ny_taxi \
-    --target_table=yellow_taxi_trips_2021_1 \
-    --chunksize=100000
+docker run -it \
+  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+  -e PGADMIN_DEFAULT_PASSWORD="root" \
+  -v pgadmin_data:/var/lib/pgadmin \
+  -p 8085:80 \
+  --network=pg-network \
+  --name pgadmin \
+  dpage/pgadmin4
